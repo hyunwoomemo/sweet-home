d7f4e2d6290b37cd8097d55bbd4b199b5324ccf4
hyunwoomemo <archihw94@gmail.com>
2023-09-19 00:09:48 +0900

    etc: 기본 셋팅

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import {NavigationContainer} from '@react-navigation/native'
+import Root from './navigators/Root';
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+
+  const startLoading = async () => {
+    setReady(true)
+  }
+
+  useEffect(() => {
+    startLoading()
+  }, [])
+
+  const onLayoutRootView = useCallback(async () => {
+    if (ready) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [ready]);
+
+  if (!ready) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer onLayout={onLayoutRootView}>
+      <StatusBar style='dark'/>
+      <Root></Root>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ readme.md
@@ -0,0 +1,7 @@
+# 집안일 협업 솔루션 🥳
+- 동거, 육아 그리고 집안일이 있는 모든 가정을 위한 애플리케이션
+
+## 프로젝트 셋팅
+
+1. Andriod, iOS firebase 셋팅 [참고](https://rnfirebase.io/)
+2. Library 설치
